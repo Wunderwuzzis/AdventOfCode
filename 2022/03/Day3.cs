2b0fdeabb959701fb144e27aba88a223a103f88d
Marlene Mayr <marlene.mayr@nextgames.com>
2022-12-03 12:17:17 +0200

    Add day3 part1

---
+++ 2022/03/Day3.cs
@@ -0,0 +1,41 @@
+﻿namespace AoC;
+
+public class Day3 : Day<int>
+{
+    public Day3(string title, int? target1 = null, int? target2 = null) : base(3, title, target1, target2) { }
+
+    protected override int Part1()
+    {
+        return Data.Sum(line =>
+        {
+            var (compartment1, compartment2) = GetCompartments(line);
+            var duplicate = FindDuplicateItemType(compartment1, compartment2);
+            return GetPriority(duplicate);
+        });
+    }
+
+    private static (char[], char[]) GetCompartments(string rucksack)
+    {
+        var half = (int) (rucksack.Length * 0.5);
+        return (rucksack[..half].ToCharArray(), rucksack[half..].ToCharArray());
+    }
+
+    private static char FindDuplicateItemType(IEnumerable<char> compartment1, IEnumerable<char> compartment2)
+    {
+        return compartment1.FirstOrDefault(compartment2.Contains);
+    }
+
+    private static int GetPriority(char character)
+    {
+        if (char.IsLower(character))
+            return character - 'a' + 1;
+        if (char.IsUpper(character))
+            return character - 'A' + 1 + 26;
+        throw new ArgumentException("Can't get type priority for non-letter character");
+    }
+
+    protected override int Part2()
+    {
+        return 0;
+    }
+}
--- 2022/AoC2022.cs
+++ 2022/AoC2022.cs
@@ -5,7 +5,8 @@ Console.WriteLine("Hello, AOC 2022!");
 Dictionary<int, IDay> days = new()
 {
     [1] = new Day1("Calorie Counting", 71471, 211189),
-    [2] = new Day2("Rock Paper Scissors", 12772, 11618)
+    [2] = new Day2("Rock Paper Scissors", 12772, 11618),
+    [3] = new Day3("Rucksack Reorganization", 7821)
 };
 
 AdventOfCode.Run(days[DateTime.Today.Day]);
