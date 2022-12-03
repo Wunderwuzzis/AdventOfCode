ac967882438b7219cae98e7fd3c66d6bff82d44e
Marlene Mayr <marlene.mayr@nextgames.com>
2022-12-03 13:07:47 +0200

    Add condensed version of day3

---
+++ 2022/03/Day3Short.cs
@@ -0,0 +1,26 @@
+﻿namespace AoC;
+
+public class Day3Short : Day<int>
+{
+    public Day3Short(string title, int? target1 = null, int? target2 = null) : base(3, title, target1, target2) { }
+
+    protected override int Part1()
+    {
+        return Data
+            .Sum(l =>
+            {
+                var c = l[..(l.Length / 2)].First(l[(l.Length / 2)..].Contains);
+                return char.IsLower(c) ? c - 'a' + 1 : c - 'A' + 1 + 26;
+            });
+    }
+
+    protected override int Part2()
+    {
+        return Data.Chunk(3)
+            .Sum(g =>
+            {
+                var c = g[0].First(c => g[1].Contains(c) && g[2].Contains(c));
+                return char.IsLower(c) ? c - 'a' + 1 : c - 'A' + 1 + 26;
+            });
+    }
+}
--- 2022/AoC2022.cs
+++ 2022/AoC2022.cs
@@ -6,7 +6,7 @@ Dictionary<int, IDay> days = new()
 {
     [1] = new Day1("Calorie Counting", 71471, 211189),
     [2] = new Day2("Rock Paper Scissors", 12772, 11618),
-    [3] = new Day3("Rucksack Reorganization", 7821, 2752),
+    [3] = new Day3Short("Rucksack Reorganization", 7821, 2752),
 };
 
 AdventOfCode.Run(days[DateTime.Today.Day]);
