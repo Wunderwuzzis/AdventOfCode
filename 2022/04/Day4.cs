53e220487d8e6d2bd79ea5b435abe26a6b0a88fb
Marlene Mayr <marlene.mayr@nextgames.com>
2022-12-04 12:45:19 +0200

    Add day4

---
+++ 2022/04/Day4.cs
@@ -0,0 +1,28 @@
+﻿using AoC.Utils;
+
+namespace AoC;
+
+public class Day4 : Day<int>
+{
+    public Day4(string title, int? target1 = null, int? target2 = null) : base(4, title, target1, target2) { }
+
+    protected override int Part1()
+    {
+        return Data.Count(line =>
+        {
+            var bounds = line.ExtractAllInt(4);
+            var contains = bounds[0] <= bounds[2] && bounds[1] >= bounds[3] || bounds[0] >= bounds[2] && bounds[1] <= bounds[3];
+            return contains;
+        });
+    }
+
+    protected override int Part2()
+    {
+        return Data.Count(line =>
+        {
+            var bounds = line.ExtractAllInt(4);
+            var exclusive = bounds[0] > bounds[3] || bounds[1] < bounds[2];
+            return !exclusive;
+        });
+    }
+}
--- 2022/AoC2022.cs
+++ 2022/AoC2022.cs
@@ -7,6 +7,7 @@ Dictionary<int, IDay> days = new()
     [1] = new Day1("Calorie Counting", 71471, 211189),
     [2] = new Day2("Rock Paper Scissors", 12772, 11618),
     [3] = new Day3Short("Rucksack Reorganization", 7821, 2752),
+    [4] = new Day4("Camp Cleanup", 450),
 };
 
 AdventOfCode.Run(days[DateTime.Today.Day]);
