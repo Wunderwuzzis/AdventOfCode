301fffcea5c637e618a1cc0051b1147e8a4e18ee
Marlene Mayr <marlene.mayr@nextgames.com>
2022-12-07 15:19:05 +0200

    Move recursions to day7 implementation

--- 2022/07/Day7.cs
+++ 2022/07/Day7.cs
@@ -6,7 +6,7 @@ namespace AoC;
 public class Day7 : Day<long, long?>
 {
     private readonly Directory _root = new("/");
-    private Directory _currentDirectory;
+    private readonly Directory _currentDirectory;
 
     public Day7(string title, long? target1, long? target2) : base(7, title, target1, target2)
     {
@@ -23,15 +23,13 @@ public class Day7 : Day<long, long?>
                 case "$" when commands[1] == "ls":
                     break;
                 case "dir":
-                    _currentDirectory.Directories.Add(new Directory(commands[1]));
+                    _currentDirectory.SubDirectories.Add(new Directory(commands[1]));
                     break;
                 default:
                     _currentDirectory.Files.Add(new File(commands[1], long.Parse(commands[0])));
                     break;
             }
         }
-
-        // _root.Print();
     }
 
     private Directory ChangeDirectory(string target) => target switch
@@ -45,8 +43,17 @@ public class Day7 : Day<long, long?>
     protected override long Part1()
     {
         var smallDirectories = new List<Directory>();
-        _root.FindAllUnder100000(ref smallDirectories);
+        FindAllUnder100000(_root, smallDirectories);
         return smallDirectories.Sum(d => d.GetSize());
+    }
+
+    private static void FindAllUnder100000(Directory dir, ICollection<Directory> results)
+    {
+        if (dir.GetSize() <= 100000)
+            results.Add(dir);
+
+        foreach (var subDir in dir.SubDirectories)
+            FindAllUnder100000(subDir, results);
     }
 
     protected override long Part2()
@@ -54,7 +61,17 @@ public class Day7 : Day<long, long?>
         var minimum = _root.GetSize() - 40000000; // max allowed space
         long smallestAboveMinimum = 70000000; // 70000000 == max disc space
 
-        _root.FindSmallestAboveMinSize(ref smallestAboveMinimum, minimum);
+        FindSmallestAboveMinSize(_root, minimum, ref smallestAboveMinimum);
         return smallestAboveMinimum;
+    }
+
+    private static void FindSmallestAboveMinSize(Directory dir, long minSize, ref long smallest)
+    {
+        var size = dir.GetSize();
+        if (size >= minSize && size < smallest)
+            smallest = size;
+
+        foreach (var subDir in dir.SubDirectories)
+            FindSmallestAboveMinSize(subDir, minSize, ref smallest);
     }
 }
--- 2022/07/Directory.cs
+++ 2022/07/Directory.cs
@@ -2,56 +2,31 @@
 
 public class Directory
 {
-    private readonly string _id;
-    private readonly HashSet<Directory> _directories;
-    private readonly HashSet<File> _files;
-
-    public string Id => _id;
-    public HashSet<Directory> Directories => _directories;
-    public HashSet<File> Files => _files;
+    private string Id { get; }
+    public HashSet<Directory> SubDirectories { get; } = new();
+    public HashSet<File> Files { get; } = new();
 
     public Directory(string id)
     {
-        _id = id;
-        _files = new HashSet<File>();
-        _directories = new HashSet<Directory>();
-    }
-
-    public void FindAllUnder100000(ref List<Directory> results)
-    {
-        if (GetSize() <= 100000)
-            results.Add(this);
-
-        foreach (var directory in Directories)
-            directory.FindAllUnder100000(ref results);
-    }
-
-    public void FindSmallestAboveMinSize(ref long smallest, long minSize)
-    {
-        var size = GetSize();
-        if (size >= minSize && size < smallest)
-            smallest = size;
-
-        foreach (var directory in Directories)
-            directory.FindSmallestAboveMinSize(ref smallest, minSize);
+        Id = id;
     }
 
     public long GetSize()
     {
-        return Files.Sum(f => f.Size) + Directories.Sum(d => d.GetSize());
+        return Files.Sum(f => f.Size) + SubDirectories.Sum(d => d.GetSize());
     }
 
     public Directory GetParentOf(Directory target)
     {
-        return FindParent(target) ?? throw new NullReferenceException($"No parent found for directory: {target._id}.");
+        return FindParent(target) ?? throw new NullReferenceException($"No parent found for directory: {target.Id}.");
     }
 
     private Directory? FindParent(Directory target)
     {
-        if (Directories.Contains(target))
+        if (SubDirectories.Contains(target))
             return this;
 
-        foreach (var directory in Directories)
+        foreach (var directory in SubDirectories)
         {
             var d = directory.FindParent(target);
             if (d != null)
@@ -63,21 +38,7 @@ public class Directory
 
     public Directory GetSubDirectory(string target)
     {
-        return Directories.FirstOrDefault(d => d._id == target)
+        return SubDirectories.FirstOrDefault(d => d.Id == target)
                ?? throw new NullReferenceException($"No directory found with name: {target}.");
-    }
-
-    public void Print(string indent = "")
-    {
-        Console.WriteLine($"{indent}- {_id}/");
-        indent = $"{indent}  ";
-
-        foreach (var dir in Directories)
-            dir.Print(indent);
-
-        foreach (var file in Files)
-            Console.WriteLine($"{indent}- {file.Filename}, size: {file.Size}");
-
-        indent = indent[..^2];
     }
 }
