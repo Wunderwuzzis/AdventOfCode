68eb5c2dbbbf7308e6f8115b1f32809dd6205837
Marlene Mayr <marlene.mayr@nextgames.com>
2022-12-11 14:41:09 +0200

    Clean up day11

--- 2022/11/Day11.cs
+++ 2022/11/Day11.cs
@@ -1,15 +1,15 @@
 ﻿namespace AoC;
 
-public class Day11 : Day<ulong>
+public class Day11 : Day<long>
 {
-    public Day11(string title, ulong target1 = default, ulong target2 = default) : base(11, title, target1, target2) { }
+    public Day11(string title, long target1 = default, long target2 = default) : base(11, title, target1, target2) { }
 
-    protected override void ExecuteParts(out ulong part1, out ulong part2)
+    protected override void ExecuteParts(out long part1, out long part2)
     {
         var monkeys = GetMonkeys();
         for (var i = 0; i < 20; i++)
             foreach (var monkey in monkeys)
-            foreach (var (item, target) in monkey.Throw(item => (ulong) Math.Floor(item / 3f)))
+            foreach (var (item, target) in monkey.Throw(item => (long) Math.Floor(item / 3f)))
                 monkeys[target].Items.Add(item);
 
         part1 = GetMonkeyBusiness(monkeys);
@@ -28,42 +28,19 @@ public class Day11 : Day<ulong>
     {
         return new[]
         {
-            new Monkey(new List<ulong> { 98, 97, 98, 55, 56, 72 }, 11, 4, 7, item => item * 13),
-            new Monkey(new List<ulong> { 73, 99, 55, 54, 88, 50, 55 }, 17, 2, 6, item => item + 4),
-            new Monkey(new List<ulong> { 67, 98 }, 5, 6, 5, item => item * 11),
-            new Monkey(new List<ulong> { 82, 91, 92, 53, 99 }, 13, 1, 2, item => item + 8),
-            new Monkey(new List<ulong> { 52, 62, 94, 96, 52, 87, 53, 60 }, 19, 3, 1, item => item * item),
-            new Monkey(new List<ulong> { 94, 80, 84, 79 }, 2, 7, 0, item => item + 5),
-            new Monkey(new List<ulong> { 89 }, 3, 0, 5, item => item + 1),
-            new Monkey(new List<ulong> { 70, 59, 63 }, 7, 4, 3, item => item + 3)
+            new Monkey(new List<long> { 98, 97, 98, 55, 56, 72 }, 11, 4, 7, item => item * 13),
+            new Monkey(new List<long> { 73, 99, 55, 54, 88, 50, 55 }, 17, 2, 6, item => item + 4),
+            new Monkey(new List<long> { 67, 98 }, 5, 6, 5, item => item * 11),
+            new Monkey(new List<long> { 82, 91, 92, 53, 99 }, 13, 1, 2, item => item + 8),
+            new Monkey(new List<long> { 52, 62, 94, 96, 52, 87, 53, 60 }, 19, 3, 1, item => item * item),
+            new Monkey(new List<long> { 94, 80, 84, 79 }, 2, 7, 0, item => item + 5),
+            new Monkey(new List<long> { 89 }, 3, 0, 5, item => item + 1),
+            new Monkey(new List<long> { 70, 59, 63 }, 7, 4, 3, item => item + 3)
         };
     }
 
-    private static ulong GetMonkeyBusiness(IEnumerable<Monkey> monkeys)
+    private static long GetMonkeyBusiness(IEnumerable<Monkey> monkeys)
     {
         return monkeys.Select(m => m.InspectionCount).OrderByDescending(x => x).Take(2).Aggregate((a, b) => a * b);
-    }
-
-    private record Monkey(List<ulong> Items, uint Divisor, uint TargetIfTrue, uint TargetIfFalse, Func<ulong, ulong> Inspect)
-    {
-        public ulong InspectionCount { get; private set; }
-
-        public IEnumerable<(ulong, uint)> Throw(Func<ulong, ulong>? decreaseWorry)
-        {
-            var inspectedItems = Items.Select(item => Inspect(item)).ToArray();
-            foreach (var inspectedItem in inspectedItems)
-            {
-                InspectionCount++;
-                var worry = decreaseWorry?.Invoke(inspectedItem) ?? inspectedItem;
-                yield return (worry, GetThrowTarget(worry));
-            }
-
-            Items.Clear();
-        }
-
-        private uint GetThrowTarget(ulong item)
-        {
-            return item % Divisor == 0 ? TargetIfTrue : TargetIfFalse;
-        }
     }
 }
---
+++ 2022/11/Monkey.cs
@@ -0,0 +1,39 @@
+﻿namespace AoC;
+
+public class Monkey
+{
+    private readonly int _targetIfTrue;
+    private readonly int _targetIfFalse;
+    private readonly Func<long, long> _inspect;
+    public List<long> Items { get; }
+    public int Divisor { get; }
+
+    public Monkey(List<long> items, int divisor, int targetIfTrue, int targetIfFalse, Func<long, long> inspect)
+    {
+        _targetIfTrue = targetIfTrue;
+        _targetIfFalse = targetIfFalse;
+        _inspect = inspect;
+        Items = items;
+        Divisor = divisor;
+    }
+
+    public long InspectionCount { get; private set; }
+
+    public IEnumerable<(long, int)> Throw(Func<long, long>? decreaseWorry)
+    {
+        var inspectedItems = Items.Select(item => _inspect(item)).ToArray();
+        foreach (var inspectedItem in inspectedItems)
+        {
+            InspectionCount++;
+            var worry = decreaseWorry?.Invoke(inspectedItem) ?? inspectedItem;
+            yield return (worry, GetThrowTarget(worry));
+        }
+
+        Items.Clear();
+    }
+
+    private int GetThrowTarget(long item)
+    {
+        return item % Divisor == 0 ? _targetIfTrue : _targetIfFalse;
+    }
+}
